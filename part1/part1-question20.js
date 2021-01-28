4a89d89d5e2ea0d3bf70871da1f99972d1dd9cd1
nmn009 <56015500+nmn009@users.noreply.github.com>
2021-01-27 21:22:07 -0800

    Update part1-question20.js

--- part1/part1-question20.js
+++ part1/part1-question20.js
@@ -1,1 +1,11 @@
+function myFunction() {
+  var myVar = setInterval(timeFunc,1000);
+}
 
+function timeFunc() {
+  let d = new Date();
+  let time = d.toLocaleTimeString();
+  console.log(time);
+}
+
+myFunction();
